03e677ba4138ef2976398f7ad1b347796b4c8222
Arno Clerc <arnocc@live.fr>
2019-02-14 11:44:24 +0100

    fourth commit

---
+++ routes/front.js
@@ -0,0 +1,11 @@
+// Configuration du module
+    let express = require('express');
+    let router = express.Router()   ;
+
+// Configuration de la route pour principale => get
+    router.get('/', (req, res, next) => {
+        res.render('index.html');
+    });
+
+// Exporter le module
+    module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Imports
 
-// Configuration
+// Configuration du serveur
 let express = require('express');
 let path = require ('path');
 let bodyParser = require('body-parser');
@@ -11,4 +11,14 @@ let api = require('./routes/api');
 let port = 8080;
 let app = express();
 
+// Dossier statique de la partie front
+
+// Body parser
+
+// Définition des routes
+
+
 // Démarrer le serveur
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
